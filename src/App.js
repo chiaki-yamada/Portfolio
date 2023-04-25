4f2062782f0aaec6618274f2e689eeb1c32078d5
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-04-24 20:19:59 -0700

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div className="App">
+    <Routes>
+      <Route path="" element={<Header />} />
+    </Routes>
       
     </div>
   );
