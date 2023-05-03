95f4f3468e5028d8d4e97563dc846a47010a9a01
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-05-02 21:55:35 -0700

    Updated jp

--- src/components/About.js
+++ src/components/About.js
@@ -14,9 +14,11 @@ function About() {
       <h1 className='title'>ABOUT</h1>
       <div className="about-container">       
         <h2>Konnichiwa!</h2>
-        <p>Hi I’m Chiaki, a passionate front-end developer in training with a desire to start my career in the tech industry. While I don’t have direct experience in front-end development, I have gained valuable skills through working with a developer to create a new website for the organization where I was previously working for. I managed the project, helped design, created content, and maintained the site.</p>
-        <p>I obtained a <a href='https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/71780354' target="_blank" rel="noreferrer">Front End Web Development Techdegree Certificate</a> from online course, treehouse.</p>
+        <p>Hi I’m Chiaki, a self-taught web developer with a passion for creating beautiful and functional websites.</p> 
+        <p>My coding journey began when I was in my previous role as a communications coordinator. I had the opportunity to work with a front-end developer to build and design a new website from the ground up tailored to the needs of the organization. Watching the site take shape was very exciting, and it left me with a burning desire to learn more about web development. <br></br>Since then, I started taking courses to gain the skills necessary to create a website on my own and have been studying at every chance I got. Recently, I completed <a href='https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/71780354' target="_blank" rel="noreferrer">a formal degree</a> in front-end web development.</p>
+        <p>I believe my previouse role in creating social media, newsletters, and website posts also helped me home in on my eye for design and creative abilities. </p>
         <p>I’m excited to continue learning and growing as a front-end developer and look forward to contributing to exciting projects in the future!</p>
+      
       </div>
 
       <div className="skills-container">
@@ -24,10 +26,11 @@ function About() {
         <ul>
           <li>Proficient with HTML, CSS, JavaScript, React, Bootstrap</li>
           <li>Creating responsive design and mobile-first websites</li>
-          <li>Building applications using API</li>
-          <li>Utilizing AI tool such as GitHub Copilot </li>
-          <li>Proficient with WordPress to manege website</li>
+          <li>Building applications using API data</li>
+          <li>Utilizing AI tools such as GitHub Copilot </li>
+          <li>Proficient with WordPress to manage website</li>
           <li>Designing mockups and wireframes with Figma</li>
+          <li>Creating and editing images with Photoshop</li>
         </ul>
         <div className='skills-icon'>
           <img src={htmlIcon} className='icon' alt='html-icon'></img>
@@ -51,7 +54,7 @@ function About() {
             <p>Vancouver Japanese Language School and Japanese Hall, Vancouver, Canada<br></br>2022 Jun- 2023 Mar</p>
           </div>
           <div className='work-desc'>
-            <p>In charge of creating a new website for the organization with a professional web developer, managing the site using WordPress, creating posters social media posts, posters, and monthly e-newsletter.</p>
+            <p>Lead a new website launch project for a 110+ year old organization that provides day care, Japanese programs from children to adults and community events. After the new website went live, I was fully responsible for updating and maintaining the site using WordPress. I was also responsible for designing and creating social media posts, monthly e-newsletters, as well as creating posters for internal and external distribution to expand the organization's programs and activities. </p>
           </div>
         </div>
 
@@ -61,18 +64,18 @@ function About() {
             <p>Vancouver Japanese Language School and Japanese Hall, Vancouver, Canada<br></br>2021 Apr- 2022 May</p>
           </div>
           <div className='work-desc'>
-            <p>In charge of translating documents, letters, website, articles for social media from English to Japanese and vice versa, managing registrations, and responding email and calls.</p>
+            <p>In charge of translating internal and external letters, website contents, articles for e-newsletter and social media from English to Japanese and vice versa, as well as administrative tasks such as managing registrations closely working with manager and accountant.</p>
           </div>
         </div>
 
         <div className='work-experience'>
           <div className='work-title'>
             <h3>Public Servant -Public School Administration</h3>
-            <p>Yamatsuri Junior High School, Fukushima, Japan<pr></pr>2009 Apr - 2013 Mar</p>
-            <p>Tominari Elementary School, Fukushima, Japan<pr></pr>2013 Apr - 2018 Mar</p>
+            <p>Yamatsuri Junior High School, Fukushima, Japan<br></br>2009 Apr - 2013 Mar</p>
+            <p>Tominari Elementary School, Fukushima, Japan<br></br>2013 Apr - 2018 Mar</p>
           </div>
           <div className='work-desc'>
-            <p>In charge of wide range of duties for public schools. Fully responsible for financial accounting and budget management.</p>
+            <p>n charge of a wide range of duties for public schools with around 30 teachers and 200 students. Fully responsible for financial accounting and budget management and acting as the first point of contact for the school board, students’ guardians as well as visitors.</p>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='main-title-container'>
           <h1>CHIAKI<br></br>YAMADA</h1>
           <p className='main-title'>A Front-End Developer</p>
-          <p className='main-desc'>Hi! I’m Chiaki, a self-taught web developer with a passion for creating intuitive and functional websites and apps.  <br></br>
+          <p className='main-desc'>Hi! I’m Chiaki, a self-taught web developer with a passion for creating intuitive and functional websites and applications.  <br></br>
           I’m excited to continue learning and growing as a front-end developer and look forward to contributing to exciting projects in the future!
           </p>
         </div>
